bdc2e932e01f2727f78669d364242e783d54df02
Eric Dumazet <edumazet@google.com>
2017-10-26 21:21:40 -0700

    tcp: refresh tp timestamp before tcp_mtu_probe()
    In the unlikely event tcp_mtu_probe() is sending a packet, we
    want tp->tcp_mstamp being as accurate as possible.
    
    This means we need to call tcp_mstamp_refresh() a bit earlier in
    tcp_write_xmit().
    
    Fixes: 385e20706fac ("tcp: use tp->tcp_mstamp in output path")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -817,10 +817,8 @@ static void tcp_tsq_handler(struct sock *sk)
 		struct tcp_sock *tp = tcp_sk(sk);
 
 		if (tp->lost_out > tp->retrans_out &&
-		    tp->snd_cwnd > tcp_packets_in_flight(tp)) {
-			tcp_mstamp_refresh(tp);
+		    tp->snd_cwnd > tcp_packets_in_flight(tp))
 			tcp_xmit_retransmit_queue(sk);
-		}
 
 #ifdef CONFIG_MPTCP
 		tcp_sk(sk)->ops->write_xmit(sk, tcp_current_mss(sk),
@@ -2345,6 +2343,7 @@ bool tcp_write_xmit(struct sock *sk, unsigned int mss_now, int nonagle,
 		&& !mptcp(tp)
 #endif
 	) {
+                tcp_mstamp_refresh(tp);
 		/* Do MTU probing. */
 		result = tcp_mtu_probe(sk);
 		if (!result) {

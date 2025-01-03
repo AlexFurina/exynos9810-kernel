4b6bdf817ac3fbd9db99027ab81e5d894285aaf7
jsdizkcksv <3531236823@qq.com>
2025-01-04 02:23:19 +0800

    bpf: Submit net_device type

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -410,6 +410,7 @@ static inline bool unprivileged_ebpf_enabled(void)
 
 /* Map specifics */
 struct net_device  *__dev_map_lookup_elem(struct bpf_map *map, u32 key);
+struct net_device  *__dev_map_hash_lookup_elem(struct bpf_map *map, u32 key);
 struct sock  *__sock_map_lookup_elem(struct bpf_map *map, u32 key);
 void __dev_map_insert_ctx(struct bpf_map *map, u32 index);
 void __dev_map_flush(struct bpf_map *map);
